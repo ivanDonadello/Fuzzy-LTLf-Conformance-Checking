a7ca064e10c27de68cf9db8fba82c395c0241b24
Paolo Felli <>
2024-10-03 19:18:52 +0200

    no message

--- FLTLf/core.py
+++ FLTLf/core.py
@@ -4,10 +4,6 @@ from torch.masked import masked_tensor
 from itertools import chain, combinations
 
 
-# tensor_log.shape = (num_traces, num_events, num_predicates)
-tensor_log: torch.Tensor #TODO slice the tensor with 
-batch_size: int
-maxlength: int
 
 ##### propositional #####
 
