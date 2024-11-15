d918e00e4817f6e21e7d638ceb665e300956e3b3
Ivan Donadello <donadelloivan@gmail.com>
2024-11-15 14:36:34 +0100

    Update simple_exp.py

--- simple_exp.py
+++ simple_exp.py
@@ -1,13 +1,7 @@
 import time
-import csv
-import numpy as np
 import torch
-import math
+import pandas as pd
 import pdb
-#import matplotlib.pyplot as plt
-#import pandas as pd
-
-
 
 from FLTLf.parser import LTLfParser
 from FLTLf.converter import Converter
@@ -18,7 +12,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-d', "--device",)
+parser.add_argument('-d', "--device", default="cpu")
 
 args = parser.parse_args()
 core.device = args.device
@@ -26,144 +20,113 @@ core.debug = False
 
 torch.manual_seed(0)
 
-case_length_list = [i for i in range(500, 3500, 500)]
-log_length_list = [100, 1000, 10000, 100000]
-num_runs = 11
+num_runs = 10
+results = {"Run" : [], "Num. Cases" : [], "Case Length" : []}
+batch_sizes_column = []
+maxlengths_column = []
+run_column = []
 
-#case_length_list = [3000]
-#log_length_list = [50000]
+i = 0 
+maxlengths = [i for i in range(500, 3500, 500)]
+batch_sizes = [100, 1000, 10000]
+num_AP = 2
+predicate_names = [f"p{i}" for i in range(num_AP)]
 
+simple_formulas = ["(X(p0) >= 0.5)", "(WX(p0) >= 0.5)", "(G(p0) >= 0.5)", "(F(p0) >= 0.5)", "((p0Up1) >= 0.5)", "((p0Wp1) >= 0.5)", "((p0Rp1) >= 0.5)", "((p0Mp1) >= 0.5)"]
 
-results = []
-predicate_names = ["a", "b", "c"]
+results_per_formula = {formula : [] for formula in simple_formulas}
 
-i = 0         # TODO togliere da input
-simple_formulas = ["(X(a) >= 0.5)", "(X(a) >= 0.5)", "(WX(a) >= 0.5)", "(G(a) >= 0.5)", "(F(a) >= 0.5)", "((aUb) >= 0.5)", "((aWb) >= 0.5)", "((aRb) >= 0.5)", "((aMb) >= 0.5)"]
-#simple_formulas = ["(G(a -> F(b))) > 0.2"]
-#simple_formulas = ["((aM(aUb)) >= 0.1)"]
+#verbose printouts
+verbose = False
+#save results on a csv
+savefile = True
+#padding needed?
+skippadding = True
 
-header = ["Run", "Num. Cases", "Case Length"] + simple_formulas
+#for every combination of maxlength,batchsize
+for batch_size in batch_sizes:
+    for maxlength in maxlengths:
+        
+        run_column.extend([i for i in range(num_runs)])
+        batch_sizes_column.extend([batch_size]*num_runs)
+        maxlengths_column.extend([maxlength]*num_runs)
 
-for log_length in log_length_list:
-    for case_length in case_length_list:
-        tensor_log = torch.rand(log_length, case_length, len(predicate_names))
+        print("------times-------")
+        start = time.time()
 
-        max_t = tensor_log.shape[1]
-        batch_size = tensor_log.shape[0]
+        #generate a random tensor log
+        tensor_log = torch.rand(batch_size, maxlength, len(predicate_names), dtype=torch.half)
+        rand = time.time()
+        print(f"RANDOM GEN time {rand - start}")
 
-        traces = tensor_log.tolist()
-        #print(traces)
+        #padding
+        converter = Converter(predicate_names)    
+        tensor_log = converter.log2tensor(tensor_log,verbose,skippadding) 
 
-        # preliminary log manipulation and padding 
-        converter: Converter = Converter(predicate_names, traces)      # TODO togliere input.predicate_names, input.traces
-
-        print("Log conversion...")
-        tensor_log = converter.log2tensor(verbose=False)    # TODO togliere input.formula, non si può fare lo slicing dei predicati altrove?
+        if not(skippadding):
+            print(f"PADDING time {time.time() - rand}")
+        else:
+            print(f"PADDING skipped")        
 
         # Parsing into a formula
         parser = LTLfParser()
-
-        for run in range(num_runs):
-            results_per_formula = [run, log_length, case_length]
-            for formula in simple_formulas:
+        
+        #for each formula
+        
+        for formula in simple_formulas:
+      
+            start = time.time()
                 
+            #slicing of predicates not in formula
+            #the core routine reads the predicate_names from input, so we override them
+            core.tensor_log, input.predicate_names = converter.slice_tensor_log(tensor_log, formula, verbose)
+            core.tensor_log = core.tensor_log.to(core.device)
+                
+            #setting dimensions for practical access
+            core.batch_size = converter.batch_size
+            core.maxlength = converter.maxlength
+
+            ready = time.time()
+            print(f"SLICING TIME {ready - start}")
+
+            try:
+                pyformula = parser(formula)     
+            except Exception as e:
+                print(traceback.format_exc())
+
+            print(f"PARSING TIME {time.time() - ready}")
+                            
+                
+            for run in range(num_runs):
+
                 start = time.time()
-                # prepares the log, also slicing out predicates not in the formula
-                core.tensor_log, input.predicate_names = converter.slice_tensor_log(tensor_log, formula, verbose=False)
-                core.tensor_log = core.tensor_log.to(core.device)
 
-                # number of log traces
-                core.batch_size = converter.batch_size
+                visitor = core.Visitor()  #nuovo ogni volta?
 
-                # length of longest trace
-                core.maxlength = converter.maxlength
-
+                print("------------------")
+                print(f"Evaluation of {pyformula.print()} at instant {i} :")
                 try:
-                    pyformula = parser(formula)      # TODO togliere input.formula
-                    print(f"Evaluation of {pyformula.print()} at instant {i} :")
-                    visitor = core.Visitor() 
                     visitor.visit(pyformula, i)
-
-                    #_ = pyformula.eval(i)
+                    #pyformula.eval(i) - old evaluation code
                 except Exception as e:
                     print(traceback.format_exc())
 
                 end = time.time()
                 exec_time = end - start
-                results_per_formula.append(exec_time)
-                print(f"({run}, {log_length}, {case_length}) -> {exec_time}")
 
-            results.append(results_per_formula)
+                if savefile:
+                    results_per_formula[formula].append(exec_time)
 
-with open('experiments/results/simple_conformance_check_times.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(header)
-    for idx, res in enumerate(results):
-        writer.writerow(res)
+                print(f"({run}, {core.batch_size}, {core.maxlength}) -> {exec_time}")
 
 
-"""
-i = 0         # TODO togliere da input
-simple_formulas = ["(X(a) >= 0.5)", "(X(a) >= 0.5)", "(WX(a) >= 0.5)", "(G(a) >= 0.5)", "(F(a) >= 0.5)", "((aUb) >= 0.5)", "((aWb) >= 0.5)", "((aRb) >= 0.5)", "((aMb) >= 0.5)"]
-simple_formulas = ["((aWb) >= 0.5)"]
+results["Run"] = run_column
+results["Num. Cases"] = batch_sizes_column
+results["Case Length"] = maxlengths_column
 
-header = ["Run", "Num. Cases", "Case Length"] + simple_formulas
-  
-for log_length in log_length_list:
-    for case_length in case_length_list:
-        tensor_log = torch.rand(log_length, case_length, len(predicate_names))
+results_df = pd.DataFrame.from_dict(results_per_formula)
+settings_df = pd.DataFrame.from_dict(results)
+results_df = pd.concat([settings_df, results_df], axis=1)
 
-        max_t = tensor_log.shape[1]
-        batch_size = tensor_log.shape[0]
-        
-        traces = tensor_log.tolist()
-        #print(traces)
-
-        # preliminary log manipulation and padding 
-        converter: Converter = Converter(predicate_names, traces)      # TODO togliere input.predicate_names, input.traces
-        
-        print("Log conversion...")
-        tensor_log = converter.log2tensor(verbose=False)    # TODO togliere input.formula, non si può fare lo slicing dei predicati altrove?
-
-        # prepares the log, also slicing out predicates not in the formula
-        core.tensor_log, input.predicate_names = converter.slice_tensor_log(tensor_log, formula, verbose=False)
-                        
-        # number of log traces
-        core.batch_size = converter.batch_size
-            
-        # length of longest trace
-        core.maxlength = converter.maxlength
-
-        # Parsing into a formula
-        parser = LTLfParser()
-        
-        for run in range(num_runs):
-            results_per_formula = [run, log_length, case_length]
-            for formula in simple_formulas:
-            
-                start = time.time()
-                        
-                try:
-                    pyformula = parser(formula)      # TODO togliere input.formula
-                    print(f"Evaluation of {pyformula.print()} at instant {i} :")
-                    
-                    visitor = core.Visitor() 
-                    visitor.visit(pyformula, i)
-
-                    _ = pyformula.eval(i)
-                except Exception as e:
-                    print(traceback.format_exc())
-                
-                end = time.time()
-                exec_time = end - start
-                results_per_formula.append(exec_time)
-                print(f"({run}, {log_length}, {case_length}) -> {exec_time}")
-
-            results.append(results_per_formula)
-        
-with open('experiments/results/simple_conformance_check_times.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(header)
-    for idx, res in enumerate(results):
-	    writer.writerow(res)
-"""
+if savefile:
+    results_df.to_csv(os.path.join('experiments', 'results', f'simple_conformance_check_times_{core.device.upper()}.csv'))
